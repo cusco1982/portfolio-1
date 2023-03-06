ab97bbfbc77d383d6e5ba4a0569550d1e404f783
Julian <cusco2891@gmail.com>
2023-03-05 19:50:12 -0500

    nextjs template

---
+++ .next/server/font-loader-manifest.js
@@ -0,0 +1,10 @@
+self.__FONT_LOADER_MANIFEST={
+  "pages": {},
+  "app": {
+    "/Users/abstract/Desktop/projects/portfolio-1/node_modules/next/font/google/target.css?{\"path\":\"src/app/page.js\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}": [
+      "static/media/2aaf0723e720e8b9-s.p.woff2"
+    ]
+  },
+  "appUsingSizeAdjust": true,
+  "pagesUsingSizeAdjust": false
+}
